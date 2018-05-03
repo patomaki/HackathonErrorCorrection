d81524b6c7ec54e4373175e938561791c1b1dede
Alexowens94 <33317259+Alexowens94@users.noreply.github.com>
2018-05-03 19:42:34 +0100

    Add files via upload
    added the correction function

--- steane_code.py
+++ steane_code.py
@@ -1,6 +1,52 @@
-from pyquil.gates import H,CNOT,X,Z,CZ
+from pyquil.gates import H,CNOT,X,Z,CZ,CCNOT
+import numpy as np
 
 #-----Function definitions-----#
+
+def def_CCCNOT(p):
+    '''
+        Define a triple control not gate to program p
+    '''
+    CCCNOT = np.array([[1.,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,1.,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1.,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,1.,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,1.,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,1.,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,1.,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,1.,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,1.,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,1.,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,1.,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,1.,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,1.,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,1.,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1.],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1.,0]])
+    p.defgate("CCCNOT",CCCNOT)
+
+def def_CCCZ(p):
+    '''
+        Define a triple control not gate to program p
+    '''
+    CCCZ = np.array([[1.,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,1.,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1.,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,1.,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,1.,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,1.,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,1.,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,1.,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,1.,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,1.,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,1.,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,1.,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,1.,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,1.,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1.,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1.]])
+    p.defgate("CCCZ",CCCZ)
+
 def encode_steane_code_qubit(code_indices,data_q_index,p):
     '''
     Append p with Steane code encoding circuit for qubits
@@ -104,23 +150,60 @@ def syndrome_circuit(ancilla_indices,code_indices,p):
     syn_p.inst([H(q) for q in ancilla_indices])
     syn_p.inst([CNOT(ancilla_indices[0],q)
                 for q in code_indices[3:7]])
-    syn_p.inst([CNOT(ancilla_indices[1],code_indices[0]),
-                CNOT(ancilla_indices[1],code_indices[1])])
-    syn_p.inst([CNOT(ancilla_indices[1],code_indices[4]),
-                CNOT(ancilla_indices[1],code_indices[5])])
-    syn_p.inst([CNOT(ancilla_indices[2],q) for q in code_indices if q%2==0])
+    syn_p.inst([CNOT(ancilla_indices[1],code_indices[1]),
+                CNOT(ancilla_indices[1],code_indices[2])])
+    syn_p.inst([CNOT(ancilla_indices[1],code_indices[5]),
+                CNOT(ancilla_indices[1],code_indices[6])])
+    #syn_p.inst([CNOT(ancilla_indices[2],q) for q in range(len(code_indices)) if q%2==0])
+    syn_p.inst([CNOT(ancilla_indices[2],code_indices[0]),
+                CNOT(ancilla_indices[2],code_indices[2])])
+    syn_p.inst([CNOT(ancilla_indices[2],code_indices[4]),
+                CNOT(ancilla_indices[2],code_indices[6])])
+
     syn_p.inst([CZ(ancilla_indices[3],q)
                 for q in code_indices[3:7]])
-    syn_p.inst([CZ(ancilla_indices[4],code_indices[0]),
-                CZ(ancilla_indices[4],code_indices[1])])
-    syn_p.inst([CZ(ancilla_indices[4],code_indices[4]),
-                CZ(ancilla_indices[4],code_indices[5])])
-    syn_p.inst([CZ(ancilla_indices[5],q) for q in code_indices if q%2==0])
+    syn_p.inst([CZ(ancilla_indices[4],code_indices[1]),
+                CZ(ancilla_indices[4],code_indices[2])])
+    syn_p.inst([CZ(ancilla_indices[4],code_indices[5]),
+                CZ(ancilla_indices[4],code_indices[6])])
+    #syn_p.inst([CZ(ancilla_indices[5],q) for q in range(len(code_indices)) if q%2==0])
+    syn_p.inst([CZ(ancilla_indices[5],code_indices[0]),
+                CZ(ancilla_indices[5],code_indices[2])])
+    syn_p.inst([CZ(ancilla_indices[5],code_indices[4]),
+                CZ(ancilla_indices[5],code_indices[6])])
     syn_p.inst([H(q) for q in ancilla_indices])
     return syn_p
 # #
 # #
+def conditional_error_correction(ancilla_indices,code_indices,p):
+    '''
+    '''
+    new_p = p
 
+    # 1-qubit X-errors
+    #new_p.inst(CNOT(ancilla_indices[3],code_indices[3]))
+    #new_p.inst(CNOT(ancilla_indices[4],code_indices[1]))
+    #new_p.inst(CNOT(ancilla_indices[5],code_indices[0]))
+    new_p.inst(("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[6]))
+    new_p.inst(X(ancilla_indices[3]),("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[2]),X(ancilla_indices[3]))
+    new_p.inst(X(ancilla_indices[4]),("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[4]),X(ancilla_indices[4]))
+    new_p.inst(X(ancilla_indices[5]),("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[5]),X(ancilla_indices[5]),)
+    new_p.inst(X(ancilla_indices[3]),X(ancilla_indices[4]),("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[0]),X(ancilla_indices[3]),X(ancilla_indices[4]))
+    new_p.inst(X(ancilla_indices[3]),X(ancilla_indices[5]),("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[1]),X(ancilla_indices[3]),X(ancilla_indices[5]))
+    new_p.inst(X(ancilla_indices[4]),X(ancilla_indices[5]),("CCCNOT",ancilla_indices[3],ancilla_indices[4],ancilla_indices[5],code_indices[3]),X(ancilla_indices[4]),X(ancilla_indices[5]))
 
+    # 1-qubit Z-errors
+    #new_p.inst(CZ(ancilla_indices[0],code_indices[3]))
+    #new_p.inst(CZ(ancilla_indices[1],code_indices[1]))
+    #new_p.inst(CZ(ancilla_indices[2],code_indices[0]))
+    new_p.inst(("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[6]))
+    new_p.inst(X(ancilla_indices[0]),("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[2]),X(ancilla_indices[0]))
+    new_p.inst(X(ancilla_indices[1]),("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[4]),X(ancilla_indices[1]))
+    new_p.inst(X(ancilla_indices[2]),("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[5]),X(ancilla_indices[2]),)
+    new_p.inst(X(ancilla_indices[0]),X(ancilla_indices[1]),("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[0]),X(ancilla_indices[0]),X(ancilla_indices[1]))
+    new_p.inst(X(ancilla_indices[0]),X(ancilla_indices[2]),("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[1]),X(ancilla_indices[0]),X(ancilla_indices[2]))
+    new_p.inst(X(ancilla_indices[1]),X(ancilla_indices[2]),("CCCZ",ancilla_indices[0],ancilla_indices[1],ancilla_indices[2],code_indices[3]),X(ancilla_indices[1]),X(ancilla_indices[2]))
+    
+    return new_p
 
 
--- steane_code_main.py
+++ steane_code_main.py
@@ -1,35 +1,31 @@
 from steane_code import *
 from pyquil.quil import Program
-from pyquil.api import QVMConnection,get_devices
+from pyquil.api import QVMConnection
 #-----Main function-----#
 def main():
     #---Constants---#
-    # dead: 2,3,15,18
-    code_indices = [1,7,12,17,11,6,16]
-    # code_indices    = [0,1,4,5,6,7,8]
-    # data_q_index    = 1
-    # ancilla_indices = [9,10,11,12,13,14]
-    
+    code_indices    = [0,1,2,4,5,6,7]
+    data_q_index    = 0
+    ancilla_indices = [8,9,10,11,12,13]
+    p = Program()
     qvm = QVMConnection()
-    # acorn = get_devices(as_dict=True)['19Q-Acorn']
-    # qvm = QVMConnection(acorn)  #QVM with QPU noise
-
-    syndrome_table = []
-    for e in range(2*len(code_indices)):
-        p = Program()
-        p = encode_steane_code_qubit(code_indices,data_q_index,p)
-        if e < 7:
-            p.inst(X(code_indices[e%7]))
-        else:
-            p.inst(Z(code_indices[e%7]))
-        p = syndrome_circuit(ancilla_indices,code_indices,p)
-        p = decode_steane_code_qubit(code_indices,data_q_index,p)
-        p.measure_all(*[(q,q) for q in ancilla_indices])
-        run_results = qvm.run(p,ancilla_indices)
-        ket_results = qvm.wavefunction(p)
-        syndrome_table.append(run_results)
-    print(syndrome_table)
-
+    def_CCCNOT(p)
+    def_CCCZ(p)
+    p.inst(X(0))
+    p = encode_steane_code_qubit(code_indices,data_q_index,p)
+    #p.inst(Z(code_indices[2]))
+    p = syndrome_circuit(ancilla_indices,code_indices,p)
+    p = conditional_error_correction(ancilla_indices,code_indices,p)
+    p = decode_steane_code_qubit(code_indices,data_q_index,p)
+    p.measure_all(*[(q,q) for q in code_indices+ancilla_indices])
+    #p.measure_all(*[(q,q) for q in code_indices])
+    #run_results = qvm.run(p,code_indices)
+    run_results = qvm.run(p,code_indices+ancilla_indices)
+    ket_results = qvm.wavefunction(p)
+    print('state after decoding:')
+    print(ket_results)
+    print('measurement results:')
+    print(run_results)
 # #
 # #
 if __name__ == '__main__':
